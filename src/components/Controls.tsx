541512369d741f4e8bdd4a6562219867660b45c8
etopritika <dimaprytyka@gmail.com>
2025-03-24 12:33:24 +0200

    Rename add-todo

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,11 +1,11 @@
-import AddTodo from "./AddTodo";
+import AddTodoButton from "./AddTodoButton";
 import FilterTabs from "./FilterTabs";
 
 export default function Controls() {
   return (
-    <header className="flex space-x-2">
+    <header className="flex space-x-2 justify-center">
       <FilterTabs />
-      <AddTodo />
+      <AddTodoButton />
     </header>
   );
 }
